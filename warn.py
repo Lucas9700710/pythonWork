0cae0f3409aa166616be903e5a3937ff20797e28
Lucas9700710 <pupss103309@gmail.com>
2024-06-01 22:01:28 +0800

    Create warn.py

---
+++ warn.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+win=tk.Tk()
+win.geometry("300x400")
+win.title("LucasWeb")
+
+Lable=tk.Label(win,text="HI this only a test!!!!!",background="red",font=30,height=40,width=500)
+Lable.pack(side=tk.BOTTOM)
+
+
+
+
+
+
+
+win.mainloop()
