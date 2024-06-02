dfc4f9bb8b8f5d1821615ca097dd0741ad22e50e
Lucas9700710 <pupss103309@gmail.com>
2024-06-02 14:33:23 +0800

    Update former.py

--- former.py
+++ former.py
@@ -1,6 +1,9 @@
 import pyautogui as pg
 import warn
 import json
+import time
 warn.showWarn()
-for n in range(2):
-    pg.typewrite("w")
+time.sleep(10)
+def walk(letter,step=2):
+    for n in range(step):
+        pg.typewrite("letter")
