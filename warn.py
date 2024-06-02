3d0f87dccda66f4730bbb870b6e8f4ffe930b93d
Lucas9700710 <pupss103309@gmail.com>
2024-06-02 13:53:30 +0800

    Update warn.py

--- warn.py
+++ warn.py
@@ -3,7 +3,7 @@ win=tk.Tk()
 win.geometry("300x400")
 win.title("LucasWeb")
 
-Lable=tk.Label(win,text="HI this only a test!!!!!",background="red",font=30,height=40,width=500)
+Lable=tk.Label(win,text="請開啟cmd!!!!!",background="red",font=30,height=40,width=500)
 Lable.pack(side=tk.BOTTOM)
 
 
@@ -11,5 +11,5 @@ Lable.pack(side=tk.BOTTOM)
 
 
 
-
-win.mainloop()
+def showWarn():
+    win.mainloop()
